7fb334ca3d8e607e62014165e62c4afcb2adb082
Ujang Arisandi <ujangaja@gmail.com>
2023-06-04 16:39:57 +0700

    Setup Template Admin LTE | User Page

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Stater Kit App
+
+## Setup Template Admin LTE | User Page
+
+    todo:
+    1.  components/Wrapper.jsx
+        - membuat wrapper admin lte
+    2.  App.jsx
+        - import dan panggil Navbar
+        - import dan panggil Sidebar
+        - import dan panggil ListUser
+        - import dan panggil Footer
+    3.  main.jsx
+    4.  index.html
+        - panggil plugin dan dist
+    5.  components/Navbar.jsx
+    6.  components/Sidebar.jsx
+    7.  components/Footer.jsx
+    8.  pages/user/ListUser.jsx
+    9.  hidden file plugins
+    10. pengujian pada browser:
+        - npm run dev
+        - http://127.0.0.1:5173
+        - klik User
+            http://127.0.0.1:5173/users
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <>
+      <aside className='main-sidebar sidebar-dark-primary elevation-4'>
+        {/* <!-- Brand Logo --> */}
+        <a href='../../index3.html' className='brand-link'>
+          <img
+            src='../../public/images/logo staterkit 2.png'
+            alt='AdminLTE Logo'
+            className='brand-image'
+            // style={{ opacity: '.8' }}
+          />
+          <span className='brand-text font-weight-light'>Stater Kit App</span>
+        </a>
+
+        {/* <!-- Sidebar --> */}
+        <div className='sidebar'>
+          {/* <!-- Sidebar user (optional) --> */}
+          <div className='user-panel mt-3 pb-3 mb-3 d-flex'>
+            <div className='image'>
+              <img
+                src='../../dist/img/user2-160x160.jpg'
+                className='img-circle elevation-2'
+                alt='User Image'
+              />
+            </div>
+            <div className='info'>
+              <a href='#' className='d-block'>
+                Alexander Pierce
+              </a>
+            </div>
+          </div>
+
+          {/* <!-- SidebarSearch Form --> */}
+          <div className='form-inline'>
+            <div className='input-group' data-widget='sidebar-search'>
+              <input
+                className='form-control form-control-sidebar'
+                type='search'
+                placeholder='Search'
+                aria-label='Search'
+              />
+              <div className='input-group-append'>
+                <button className='btn btn-sidebar'>
+                  <i className='fas fa-search fa-fw'></i>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* <!-- Sidebar Menu --> */}
+          <nav className='mt-2'>
+            <ul
+              className='nav nav-pills nav-sidebar flex-column'
+              data-widget='treeview'
+              role='menu'
+              data-accordion='false'
+            >
+              {/* <!-- Add icons to the links using the .nav-icon className
+               with font-awesome or any other icon font library --> */}
+              <li className='nav-item'>
+                <Link to='/' className='nav-link'>
+                  <i className='nav-icon fas fa-tachometer-alt'></i>
+                  <p>Dashboard</p>
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/users' className='nav-link'>
+                  <i className='nav-icon fas fa-user'></i>
+                  <p>User</p>
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/roles' className='nav-link'>
+                  <i className='nav-icon fas fa-key'></i>
+                  <p>Role</p>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          {/* <!-- /.sidebar-menu --> */}
+        </div>
+        {/* <!-- /.sidebar --> */}
+      </aside>
+    </>
+  )
+}
+
+export default Sidebar
